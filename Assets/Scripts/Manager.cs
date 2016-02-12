45f329079089051c651af4085a09eefad1710890
Chain Choonoi <chain777@hotmail.com>
2016-02-12 15:39:53 +0700

    create

---
+++ Assets/Scripts/Manager.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Manager : Singleton<Manager>
+{
+    protected Manager() { } // guarantee this will be always a singleton only - can't use the constructor!
+
+    public string myGlobalVar = "whatever";
+}
